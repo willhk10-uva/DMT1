bcb7367687752855f97708a58ebce86cc3f6e650
Kevin Sullivan <sullivan@virginia.edu>
2025-09-30 04:23:02 +0000

    .

--- DMT1/L00_deductiveReasoning/99_InferenceRules.lean
+++ DMT1/L00_deductiveReasoning/99_InferenceRules.lean
@@ -1,14 +1,14 @@
 /- @@@
 # Predicate Logic Inherence Rules in Lean
 
-<!-- toc -->
-
 Below are the *canonical introductions/eliminations*
 (constructors and extractors) Lean provides for each
 connective/type in predicate logic (as it's embedded
 in Lean), stated as term signatures. These types line
 up with natural-deduction inference rules (a relation
 known as the Curry Howard Correspondence, as you know).
+
+<!-- toc -->
 
 ## ⊤ (True)
 
