160c2823b47f5e41444f1dbbf9b4e1895aea1d0c
Kevin Sullivan <sullivan@virginia.edu>
2025-09-30 04:34:26 +0000

    .

--- DMT1/L00_deductiveReasoning/99_InferenceRules.lean
+++ DMT1/L00_deductiveReasoning/99_InferenceRules.lean
@@ -10,17 +10,24 @@ known as the Curry Howard Correspondence, as you know).
 
 <!-- toc -->
 
+@@@ -/
+
+/- @@@
 ## ⊤ (True)
 
 - (⊤ intro) `True.intro : True`
 - (⊤ elim)  — a proof of true is useless
+@@@ -/
 
+/- @@@
 ## ∧ (conjunction)
 
 - (intro)   `And.intro  : P → Q → P ∧ Q`
 - (elim₁)   `And.left   : P ∧ Q → P`
 - (elim₂)   `And.right  : P ∧ Q → Q`
+@@@ -/
 
+/- @@@
 ## → (implication)
 
 - (intro)   `λ : (P → Q)` is formed by `fun (_ : P) => ... : Q`
@@ -36,29 +43,39 @@ understand →, you now understand ∀ but for syntactic details.
 
 - (intro)   `Forall.intro : (∀ x, Q)` is `fun x => ... : Q`
 - (elim)    `Forall.elim  : (∀ x, Q) → Q` -- specialization (application)
+@@@ -/
 
+/- @@@
 ## ↔ (iff)
 
 - (intro)   `Iff.intro : (P → Q) → (Q → P) → (P ↔ Q)`
 - (elim→)   `Iff.mp    : (P ↔ Q) → P → Q`
 - (elim←)   `Iff.mpr   : (P ↔ Q) → Q → P`
+@@@ -/
 
+/- @@@
 ## ∨ (disjunction)
 
 - (intro₁)  `Or.inl    : P → P ∨ Q`
 - (intro₂)  `Or.inr    : Q → P ∨ Q`
 - (elim)    `Or.elim   : P ∨ Q → (P → R) → (Q → R) → R`
+@@@ -/
 
+/- @@@
 ## ¬ (negation)
 
 - (def)     `Not P := P → False`
 - (elim)    `not_elim : (P → False) → P → False`  -- by application
+@@@ -/
 
+/- @@@
 ## ∃ (existential quantifier)
 
 - (intro)   `Exists.intro : ∀ {α} {p : α → Prop} (w : α), p w → ∃ x, p x`
 - (elim)    `Exists.elim  : (∃ x, p x) → (∀ w, p w → R) → R`  -- aka `Exists.rec`
+@@@ -/
 
+/- @@@
 ## = (propositional equality)
 
 - (refl)    `rfl` / `Eq.refl  : a = a`
