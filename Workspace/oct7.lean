3de6474e7e510ea026795394d6094775a2b56f7e
willhk10-uva <xuv9vp@virginia.edu>
2025-10-09 13:42:06 +0000

    oct7

---
+++ Workspace/oct7.lean
@@ -0,0 +1,4 @@
+/- to show that p is false, you must show that p is uninhabitated,
+and in order to do that, prove p → false-/
+
+/- In order to prove an implication, you use a lambda abstraction **************-/
